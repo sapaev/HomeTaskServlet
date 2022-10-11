b90f9ea649f7736595378a4580fc899dba53b101
qasap <qa,sapaev@gmail.com>
2022-10-11 18:26:56 +0300

    Synhron

---
+++ src/main/java/com/example/hometaskservlet/Questionnaire.java
@@ -0,0 +1,77 @@
+package com.example.hometaskservlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// Req -> (S -> S) -> jsp
+
+public class Questionnaire extends HttpServlet {
+
+    public static Map <Integer,String> statisticOfIE=new HashMap<>();
+    public static Map <Integer,String> statisticOfChromeE=new HashMap<>();
+    public static Map <Integer,String> statisticOCashYes=new HashMap<>();
+
+    public static Map <Integer,String> statisticOfВDevtoolsYes=new HashMap<>();
+
+
+
+    private static int a=0;
+    private static int b=0;
+    private static int c=0;
+    private static int f=0;
+
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String q1 = request.getParameter("browser");
+        String q2 = request.getParameter("cash");
+        String q3 = request.getParameter("devtools");
+        String name=request.getParameter("name");
+
+
+        if ("a".equals(q1)) {
+            statisticOfIE.put(a+1,"Internet Explorer");
+        }
+            else{
+            statisticOfChromeE.put(b+1,"Gogle Chrome");
+
+            }
+        if ("a".equals(q2)) {
+            statisticOCashYes.put(c+1, "statisticOCashYes");
+        }
+        if ("a".equals(q3)){
+            statisticOfВDevtoolsYes.put(f+1,"statisticOfВDevtoolsYes");
+        }
+
+            HttpSession session = request.getSession(true);
+            session.setAttribute("user_answer", name);
+
+
+        response.sendRedirect("index.jsp");
+    }
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String a = request.getParameter("a");
+        HttpSession session = request.getSession(false);
+
+        if ("exit".equals(a) && (session != null))
+            session.removeAttribute("user_answer");
+
+        response.sendRedirect("index.jsp");
+    }
+
+
+
+
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,65 @@
+<%@ page import="com.example.hometaskservlet.Questionnaire" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="en">
+<head>
+    <title>Тест</title>
+
+</head>
+<body>
+
+<% String finish = (String)session.getAttribute("user_answer"); %>
+<h1>ПРивет!</h1>
+<% if (finish == null || "".equals(finish)) { %>
+<form name="test" method="post" action="/questionnaire">
+    <h2>Укажите Ваше имя:</h2>
+        <br>
+    <label><input type="text" size="40" name="name"><br></label>
+    <h2>Укажите Ваше ФИО:</h2>
+    <br>
+    <label><input type="text" size="40"></label>
+        <br>
+    <h2>Каким браузером в основном пользуетесь:</h2>
+    <Br>
+    <label><input type="radio" name="browser" value="a"> Internet Explorer</label>
+    <Br>
+    <label><input type="radio" name="browser" value="b"> Opera</label>
+    <Br>
+    <h2>Чистите ли Вы кэш перед работой:</h2>
+    <Br>
+    <label><input type="radio" name="cash" value="a"> Да</label>
+    <Br>
+    <label><input type="radio" name="cash" value="b">  Нет</label>
+    <Br>
+    <h2>Используете ли Вы консоль разработчика:</h2><Br>
+    <label><input type="radio" name="devtools" value="a"> Да</label><
+    Br>
+    <label><input type="radio" name="devtools" value="b"> Нет</label>
+    <Br>
+    <label><input type="submit" name="send" length="50"></label>
+</form>
+<% } else{ %>
+<h1>Your answers: </h1>
+Каким браузером в основном пользуетесь:
+<Br>
+GoogleChrome:
+<% int sizeOfChrome = Questionnaire.statisticOfChromeE.size(); System.out.println(sizeOfChrome); %>
+<Br>
+InternetExplorer:
+<% int sizeOfIE = Questionnaire.statisticOfIE.size(); %>
+<%System.out.println(sizeOfIE); %>
+<Br>
+Чистите ли Вы кэш перед работой:
+<% int cleanCash = Questionnaire.statisticOCashYes.size();%>
+<%System.out.println(cleanCash); %>
+<Br>
+Используете ли Вы консоль разработчика:
+<% int devTools = Questionnaire.statisticOfВDevtoolsYes.size();%>
+<%System.out.println(devTools); %>
+
+<br>Click this link to <a href="/login?a=exit">Back</a>
+
+<% } %>
+
+</body>
+</html>
